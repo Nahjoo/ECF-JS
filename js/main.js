49bd04ddb0274febf64a5de721c423fe22e952f4
DEVnahjo <nahjo@nahjo.home>
2019-01-29 16:25:53 +0100

    upgrade

--- js/main.js
+++ js/main.js
@@ -1,61 +1,60 @@
-var API_URL = "http://api-students.popschool-lens.fr/api";
+
+//declared variable
+var API_URL = "http://api-students.popschool-lens.fr/api"; // URL oh the API
 var select = document.querySelector("#select1");
 var newnamepromo = document.querySelector(".newname");
+var myDiv = document.querySelector('.list-container');
 
+//function GET Promotions
 function getpromotions() {
-    fetch(API_URL + '/promotions')
-        .then(response => response.json())
+
+    fetch(API_URL + '/promotions') // fecth get list-promotions
+        .then(response => response.json()) // builds the JavaScript value or the object described by this string.
         .then(function (promo) {
             const promotions = promo['hydra:member'];
             console.log(promotions);
 
             const list = document.querySelector(".list-container");
-            list.innerHTML = '';
+            myDiv.innerHTML = "";
+            select.innerHTML = '';
             
-
-            promotions.forEach(promot => {
+            promotions.forEach(promot => { // GET ALL promotion one by one
                 console.log(promot.name);
-                const li = document.createElement("li");
+                const li = document.createElement("li"); 
                 const link = document.createElement("a");
-                var option = document.createElement("option");
+                var option = document.createElement("option");        // create balise OPTION
 
-                
-                if(select.innerHTML != promot.name){
-                    select.appendChild(option);
-
-                    option.innerHTML = `${promot.id} ${promot.name}`;
+                if(select.innerHTML != promot.name){                  // check if the promotion are different of the promotion in the list
+                    select.appendChild(option);                       // and
+                    option.innerHTML = `${promot.id} ${promot.name}`; // add promotion in the list
                 }
                 
-                link.href = "promotion.html?id=" + promot.id;
+                link.href = "promotion.html?id=" + promot.id; // create link on list promotions
                 link.textContent = promot.name;
-                li.appendChild(link)
+                li.appendChild(link)                          // add li in link
                 list.appendChild(li);
                 
-            });
-            
-            
-            return promotions;
+            });  
         })
-    
 }
 
-
+//function create promotion
 var createpromo = document.querySelector("#create-new-promotion");
 createpromo.addEventListener('submit', createPromotion);
 
 function createPromotion(event) {
-    event.preventDefault(event);
+    event.preventDefault(event);//preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
     const nameInput = document.querySelector("#new-promotion-name");
-    const startInput = document.querySelector("#new-promotion-startdate")
-    const endInput = document.querySelector("#new-promotion-endDate")
+    const startInput = document.querySelector("#new-promotion-startdate") //declares the id of the DOM
+    const endInput = document.querySelector("#new-promotion-endDate") // recover the value of the Input in the HTML
     console.log(nameInput.value);
 
-    fetch(API_URL + '/promotions', {
-            method: 'POST',
+    fetch(API_URL + '/promotions', { // fecth get list-promotions
+            method: 'POST', // add promotion in the API
             headers: new Headers({
                 'content-type': 'application/json'
             }),
-            body: JSON.stringify({
+            body: JSON.stringify({ // converts a JavaScript value to a JSON string
 
                 name: nameInput.value,
                 startDate: startInput.value,
@@ -65,32 +64,34 @@ function createPromotion(event) {
             })
         })
 
-        .then(function (response) {
-            
-            getpromotions()
-        })
+        .then(response => response.json())
+        .then(promo => {
+
+        getpromotions(); // callback function to refresh
+        }) 
         .catch(error => console.log(error));
 }
 
 
-var modif = document.querySelector("#newpromname");
+var modif = document.querySelector("#newpromname"); //declares the id of the DOM
 modif.addEventListener("submit", modifier);
 
-function modifier(event){
+function modifier(event){ //create function modify promotions
     event.preventDefault(event);
     fetch(API_URL + '/promotions/' + select.value , {
-        method: 'PUT',
+        method: 'PUT', // modify promotion select in the API
         headers: new Headers({
             'content-type': 'application/json'
         }),
-        body: JSON.stringify({
+        body: JSON.stringify({ // converts a JavaScript value to a JSON string
             name: newnamepromo.value,
         })
     })
 
-    .then(function (response) {
+    .then(response => response.json())
+    .then(promo => {
 
-        getpromotions();
+        getpromotions(); // callback function to refresh
     })
     .catch(error => console.log(error));
 }
@@ -99,4 +100,4 @@ function modifier(event){
 
 
 
-getpromotions();
+getpromotions(); //start function getpromotions
--- js/promotion-index.js
+++ js/promotion-index.js
@@ -1,73 +1,99 @@
+// call file main.js
 require: "main.js";
 
+//declared variable
 var API_URL = "http://api-students.popschool-lens.fr/api";
-var API_URL1 = "/api/promotions/";
-
 
 const url = new URL(window.location.href);
 const params = new URLSearchParams(url.search);
-const promotionID = params.get('id');
+const promotionID = params.get('id'); // recover the ID in the URL
 console.log(promotionID);
 
 
-
+// declared button in the html
 var deletepro = document.querySelector("#deletepromo");
+
+// call the function deletepromotion in click on the button
 deletepro.addEventListener('submit', deletepromotion);
 var select = document.querySelector("#select");
+// call the function deletestudent in click on the button
 var deletestud = document.querySelector("#deletestudent");
 deletestud.addEventListener('submit', deletestudents);
+// create option
+var option = document.createElement("option");
 
 
 
+var ul = document.querySelector('#list');
+// create function GET student
 function getstudents() {
-    fetch(API_URL + '/students')
-        .then(response => response.json())
-        .then(function (studentlist) {
-            const students = studentlist['hydra:member'];
-            console.log(students)
-            students.forEach(student => {
-                if (student.promotion == API_URL1 + promotionID) {
-                    console.log(student.lastname);
-                    var option = document.createElement("option");
-                    select.appendChild(option);
-                    option.innerHTML = `${student.id} ${student.firstname} ${student.lastname}`
-                }
-            });
-        });
+    fetch(API_URL + "/promotions/" + promotionID)  // fecth get list-promotions ID
+               .then(r => r.json())
+               .then(promo =>{
+                       console.log(promo);
+                       ul.innerHTML = ""; // allows to empty the fields so as not to duplicate them
+                       select.innerHTML = "";
+                       promo.students.forEach(studentURL => { // loop on the promotion , for recover the students
+                               fetch("http://api-students.popschool-lens.fr" + studentURL) // fecth get list-student of the promotion
+                               .then(r => r.json())
+                               .then(student => { 
+                                        var li = document.createElement("li"); 
+                                        var option = document.createElement("option");
+                                        li.id = "student" + student.id; 
+                                        select.appendChild(option); // add balise OPTION in the SELECT 
+                                        option.innerHTML = `${student.id} ${student.lastname} ${student.firstname}` // add in the OPTION student.ID etc..
+                                        ul.appendChild(li);
+                                        li.innerHTML = student.id + student.lastname + student.firstname;
+                                        var deleteStudent = document.createElement("button"); // create button 
+                                        li.appendChild(deleteStudent);
+                                        deleteStudent.innerHTML = "supprimer";
+                                        deleteStudent.value = student.id; 
+                                        deleteStudent.dataset.firstname = student.firstname; // dataset provides read and write access to all custom data attributes defined on the element
+                                        deleteStudent.dataset.lastname = student.lastname;
+                                        deleteStudent.dataset.path = student['@id'];
+                                        deleteStudent.addEventListener("click", deletestudents); // call function when click on the button                                
+                               })
+                           }
+                       );
+                   }
+               );
 }
 
 
 function deletepromotion(event) {
-    event.preventDefault(event);
-    fetch(API_URL + '/promotions/' + promotionID, {
-            method: 'DELETE',
+    event.preventDefault(event); // tells the user agent that if the event is not explicitly processed, its default action should not be taken into account
+    fetch(API_URL + '/promotions/' + promotionID, { // GET the promotion.id 
+            method: 'DELETE', // delete the promotion
             headers: {
                 'Content-Type': 'application/json'
             }
         })
 
         .then(function (response) {
-            getstudents();
+            getstudents(event);
             console.log("promo sup");
         })
         .catch(error => console.log(error));
 }
 
 
-function deletestudents(event) {
-    event.preventDefault(event);
-    fetch(API_URL + '/students/' + select.value, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
 
-        .then(function (response) {
-            getstudents();
-            console.log("student supp");
-        })
-        .catch(error => console.log(error));
+function deletestudents(event) {
+        var data_target = event.target.dataset;
+        event.preventDefault(event);
+        // pop-up to confirm delete promotion
+        if( confirm("Êtes-vous certain de vouloir supprimer l'étudiant " + data_target.firstname + " " + data_target.lastname + " ?")) {
+            console.log("ok br " + data_target.firstname);
+            fetch("http://api-students.popschool-lens.fr" + data_target.path,{
+                method: "DELETE",
+            })
+            .then(function (response) {
+                var liStudent = document.querySelector("#student" + event.target.value);
+                liStudent.remove(); // delete LI selected
+                console.log("promo sup");
+            })
+            .catch(error => console.log(error))   
+        }  
 }
 
 
@@ -77,34 +103,62 @@ createstudent.addEventListener('submit', poststudent);
 
 function poststudent(event) {
     event.preventDefault(event);
-    const firstnameInput = document.querySelector("#new-firstname-stud");
+    const firstnameInput = document.querySelector("#new-firstname-stud"); // recover value of the input with id = new-firstname-studen
     const lastnameInput = document.querySelector("#new-lastname-stud")
     const birthdateInput = document.querySelector("#birthdate-stud")
     const sexeInput = document.querySelector("#sexe-stud")
     
+    
+    fetch("http://api-students.popschool-lens.fr/api/students/", { // GET all student
+        method: "POST", // create new student
+        headers: new Headers({
+            'content-type': 'application/json'
+        }),
+        body: JSON.stringify({ // converts a JavaScript value to a JSON string
 
-    fetch(API_URL + '/students', {
-            method: 'POST',
-            headers: new Headers({
-                'content-type': 'application/json'
-            }),
-            body: JSON.stringify({
-
-                firstname: firstnameInput.value,
-                lastname: lastnameInput.value,
-                birthdate: birthdateInput.value,
-                sexe: sexeInput.value,
-                promotion: API_URL1 + promotionID,
-
-            })
-        })
-
-        .then(function (response) {
-            getstudents();
+            firstname: firstnameInput.value,
+            lastname: lastnameInput.value,
+            birthdate: birthdateInput.value,
+            sex: sexeInput.value,
+            promotion: "/api/promotions/" + promotionID,
             
+
         })
-        .catch(error => console.log(error));
+    })
+    .then(function (response) {
+        console.log("student sup");
+        getstudents(event);
+        
+    })
+    .catch(error => console.log(error));    
 }
 
+var modify = document.querySelector("#modifyStud");
+modify.addEventListener("submit", putstudent);
+
+function putstudent(event) {
+    event.preventDefault(event);
+    var new_firstname = document.querySelector("#new-firstname-student") // recover value of the input with id = new-firstname-stud
+    var new_lastname = document.querySelector("#new-lastname-student") // --
+    fetch("http://api-students.popschool-lens.fr/api/students/" + select.value, { // GET student select 
+        method: "PUT", // modify the student select
+        headers: new Headers({
+            'content-type': 'application/json'
+        }),
+        body: JSON.stringify({
+
+            firstname: new_firstname.value,
+            lastname: new_lastname.value,
+            
+
+        })
+    })
+    .then(function (response) {
+        console.log("student sup");
+        getstudents(event);
+        
+    })
+    .catch(error => console.log(error));    
+}
 
 getstudents();
